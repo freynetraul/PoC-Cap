8bd985ccc60b90f90a07a1994dd4560977574ebd
freynetraul <rau2270@hotmail.com>
2024-07-09 08:38:29 -0500

    init commit

--- MicroService3/RequestSuccesfulConsumer.cs
+++ MicroService3/RequestSuccesfulConsumer.cs
@@ -9,7 +9,7 @@ namespace MicroService3
         public void MarkOrderStatus(JsonElement param)
         {
             var orderId = param.GetProperty("OrderId").GetInt32();
-           // var isSuccess = param.GetProperty("ProducIt").GetBoolean(); 
+            //var isSuccess = param.GetProperty("ProducIt").GetBoolean(); 
            // var isSuccess = param.GetProperty("ProducIt").GetInt32();
             Console.WriteLine("Fallo la entrega de la orden");
             //Realiza acciones de compensacion
